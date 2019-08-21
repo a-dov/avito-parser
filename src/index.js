56ae311a67b7fc1ce263f7a0cad8d86ee2a560a7
Khaninev Vasilii <vhaninev@neuron-team.ru>
2019-08-21 13:54:48 +0300

    add parsing by all pages

--- src/index.js
+++ src/index.js
@@ -1,35 +1,75 @@
-const {Worker} = require('worker_threads');
 const puppeteer = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
+const URL = 'https://www.avito.ru/rossiya/gruzoviki_i_spetstehnika/ekskavatory/';
 
-function runService(url) {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker('./parsePage.js', {url});
-    worker.on('message', resolve);
-    worker.on('error', reject);
-    worker.on('exit', (code) => {
-      if (code !== 0)
-        reject(new Error(`Worker stopped with exit code ${code}`));
-    })
-  })
+function processUrl(url) {
+  const reg = /p=[0-9]+/;
+  const match = url.match(reg);
+
+  if (match) {
+    return url;
+  } else if (url.indexOf('?') !== -1) {
+    return url + '&p=';
+  }
+  return url + '?p=';
 }
 
 async function run() {
   (async () => {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.avito.ru/rossiya/gruzoviki_i_spetstehnika/ekskavatory?cd=1');
+    const url = processUrl(URL);
 
-    const hrefs = await page.evaluate(() => {
-      const anchors = document.querySelectorAll('.item-description-title-link');
-      return [].map.call(anchors, a => a.href);
-    });
+    try {
+      const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 3,
+      });
 
-    for (let i = 0; i < 2; ++i) {
-      setTimeout(() => {
-        runService(hrefs[i]);
-      }, 600 * i);
+      await cluster.task(async ({ page, data: url }) => {
+        await page.goto(url);
+        await page.tap('.js-item-phone-button_card');
+        await page
+          .waitForSelector('.item-popup .js-item-phone-big-number img')
+          .then(async () => {
+            const data = await page.evaluate(() => {
+              const phoneImg = document.querySelector('.item-popup .js-item-phone-big-number img');
+              const title = document.querySelector('.title-info-title-text');
+              const price = document.querySelector('.js-item-price');
+              const description = document.querySelector('.item-description-text p');
+              const arr = [];
+              arr.push(title.innerText, /*phoneImg ? phoneImg.src : 'Нет номера', */price ? price.getAttribute('content') : 'Цена не указана', description ? description.innerText : '');
+              return arr;
+            });
+            console.log(data);
+          });
+      });
+
+      const page = await browser.newPage();
+
+      for (let i = 1; i <= 100; ++i) {
+        await page.goto(url.replace('p=', 'p=' + i));
+
+        console.log('page:', url.replace('p=', 'p=' + i));
+
+        const hrefs = await page.evaluate(() => {
+          const anchors = document.querySelectorAll('.item-description-title-link');
+          return [].map.call(anchors, a => a.href);
+        });
+
+        if (!hrefs) break;
+
+        for (let i = 0; i < 1; ++i) {
+          cluster.queue(hrefs[i]);
+        }
+      }
+
+      await cluster.idle();
+      await cluster.close();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await browser.close();
     }
-    await browser.close();
   })();
 }
 
